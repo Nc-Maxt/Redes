4406b0392b77cf5631c8c6f95bdfa73965cee6c4
matias <m.f.nunez.caceres@gmail.com>
2026-09-02 12:49:54 -0400

    Temp-Fix: No possible to establish connections to the servers, for testing using local resolver

--- Ejercicio_2/protocolos.py
+++ Ejercicio_2/protocolos.py
@@ -19,14 +19,16 @@ def parse_DNS_message(dns_message: bytes) -> dict[hex]:
     info["Additional"] = d.ar
     return info
 
-root_ip = "198.41.0.4"
+root_ip = "127.0.0.53"
+
 def resolver(mensaje_consulta: bytes, ip_addr=root_ip) -> bytes:
     #recordar que el mensaje de consulta es justamente el que recibo del cliente
     # ahora yo debo ser quien envia ese mensaje al sv para preguntar
     # para ello debo hacer otro socket para actuar como cliente
+    # como es no orientado a conexion sera un efimero
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # Como es un socket NO orientado a conexión no necesitamos conectarlo a una dirección específica
-    address = ('localhost', 5000)
+    # recuperamos el address a donde hay que mandar el mensaje
+    address = (ip_addr, 53)
     print("voy a mandar un mensaje")
     client_socket.sendto(mensaje_consulta, address)
     print("esperando mensaje")
